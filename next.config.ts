21682238196865b00c02c2e105e881b365bfc5f5
gg320324492-lgtm <gg320324492@gmail.com>
2026-02-07 14:08:52 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = "Micro-Nano-Bubble-Technology-Lab"; // 你的仓库名
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",                 // 关键：生成 out/
+  images: { unoptimized: true },    // 关键：GitHub Pages 不支持 next/image 优化服务
+
+  // 关键：因为你是 project pages，网址会带 /仓库名/
+  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
 };
 
 export default nextConfig;
