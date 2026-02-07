2d7dd785ee9cb5af773700e606f53027c7a58637
gg320324492-lgtm <gg320324492@gmail.com>
2026-02-07 21:16:57 +0800

    Delete src/lib/withBasePath.ts

--- src/lib/withBasePath.ts
+++
@@ -1,7 +0,0 @@
-export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-
-export function withBasePath(p: string) {
-  if (!p) return p;
-  const path = p.startsWith("/") ? p : `/${p}`;
-  return `${BASE_PATH}${path}`;
-}
